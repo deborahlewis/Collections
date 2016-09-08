24c3a37dda64b562927dd365dd7b8e2d859e3b54
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-09-08 08:48:10 -0400

    Progress update

--- CollectionsDemo.java
+++ CollectionsDemo.java
@@ -7,6 +7,7 @@ package collectionsdemo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CollectionsDemo {
@@ -14,41 +15,27 @@ public class CollectionsDemo {
 
     public static void main(String[] args) {
         
-        List c = new ArrayList(); //create array list
+        Collection<String> c = new LinkedList<String>();
         
-        c.add("Deborah"); //add to array
-        c.add("David"); //add to array
-        c.add("Christie"); //add to array
-        c.add("Dawson"); //add to array
-        //c.add(0, "Jasper"); //add to first position in array
+        c.add("Nikki"); //add Nikki
+        c.add("Tony"); //add Tony
+        c.add("Jasper"); //add Jasper
         
-        //c.remove("Deborah"); //remove from array
-        
-        System.out.println("count" + c); //disaplay size of arraylist
-        
-        //System.out.println("position " + c.indexOf("Jasper")); //display location of Jasper in array
-        
-        //print each item within the array using iterator
-        Iterator iterator = c.iterator(); //create iterator (found in all collections classes)
+        Iterator<String> iterator = c.iterator(); //create iterator
         int i = 1; //counter
-        while (iterator.hasNext()) { //while there are other objects within the collection
-            Object o = iterator.next(); // go to next object
-            System.out.println(i + " " + o.toString()); //print string value of object numbered
+        while (iterator.hasNext()) { //while other objects exist in the collection
+            String o = iterator.next(); 
+            if (o.startsWith("T")) {//if the object starts with T
+                c.remove(o); //remove the object
+            }
+            
+            System.out.println(i + " " + o); //display object
             i++; //increment counter
         }
-
-        c.remove("Dawson"); //remove from array
         
-        //alternative method
-        for (Object o : c) { //for object object o is in c
-            if (o.toString().startsWith("C")) {
-                c.remove(o); //remove any objects that start with the letter C
-            }
-            System.out.println(c.toString()); //print items from collection
+        for (String o : c) {
+            System.out.println(o.length());
         }
-        
-        
-        
     }
     
 }
